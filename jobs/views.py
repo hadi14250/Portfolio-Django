088bde88b4110ec443ac0cd5d3fe6bb2c4945a76
hadi14250 <hadihk2k@gmail.com>
2024-08-04 00:39:37 +0400

    finished portfolio in django

---
+++ jobs/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Job
+
+def hadi(request):
+    jobs = Job.objects
+    return (render(request, "jobs/hadi.html",  {"jobs":jobs}))
+
+def detail(request, job_id):
+    job_detail = get_object_or_404(Job, pk=job_id)
+    return (render(request, "jobs/detail.html", {"job":job_detail}))
